ac461762d45c46296e1784738e2b035c1683a22f
TranAnnie <77334244+TranAnnie@users.noreply.github.com>
2023-05-22 09:12:15 +0200

    Created react app + implementation of frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Välkommen till blabla!</h1>
+      </div>
+      <div className="App-body">
+        <div className="Border">
+          <h3>Här kan du söka på drinkar.. blabla info om appen</h3>
+        </div>
+
+        <label>Sök här:</label>
+        <input
+          className="User-input"
+          placeholder="Sök här efter drinkar här.."
+        ></input>
+      </div>
     </div>
   );
 }
