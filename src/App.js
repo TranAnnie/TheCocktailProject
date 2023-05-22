9ef477bcea14bf1c77b26c564d654ad5ee8a3548
TranAnnie <77334244+TranAnnie@users.noreply.github.com>
2023-05-22 10:14:22 +0200

    Added some css

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <h1>Välkommen till blabla!</h1>
+        <h1>Välkommen till The Cocktail!</h1>
       </div>
       <div className="App-body">
         <div className="Border">
           <h3>Här kan du söka på drinkar.. blabla info om appen</h3>
         </div>
-
-        <label>Sök här:</label>
-        <input
-          className="User-input"
-          placeholder="Sök här efter drinkar här.."
-        ></input>
       </div>
+      <label>Sök här:</label>
+      <input
+        className="User-input"
+        placeholder="Sök här efter drinkar här.."
+      ></input>
     </div>
   );
 }
