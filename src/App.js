5067096bac005d2f26dff00116cec1635fdf13e5
Benny Petersson <benny1051@gmail.com>
2023-05-22 13:37:46 +0200

    fixing API

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import { useState } from "react";
 import DrinkList from "./components/DrinkList.js";
 
 function App() {
-  const [drinks, setDrinks] = useState([]);
-
-  async function onChange(e){
-    const searchValue = e.target.value;
-    console.log(searchValue);
-    if (searchValue.length > 2) {
-      const respons = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
-      const data = await respons.json();
-      console.log(data);
-      };
-    }
-
+ 
   return (
     <div className="App">
       <div className="App-header">
@@ -26,14 +15,8 @@ function App() {
           <h3>Här kan du söka på drinkar.. blabla info om appen</h3>
         </div>
       </div>
-      <label>Sök här:</label>
-      <input
-        className="User-input"
-        placeholder="Sök här efter drinkar här.."
-        type="text"
-        onChange={onChange}
-      ></input>
-      <DrinkList drinks={drinks} />
+
+      <DrinkList />
     </div>
   );
 }
--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,14 +1,38 @@
 import DrinkItem from "./DrinkItem";
+import { useState } from "react";
 
-const DrinkList = (props) => {
+const DrinkList = () => {
+  const [drinks, setDrinks] = useState([]);
+
+  async function onChange(e) {
+    const searchValue = e.target.value;
+    console.log(searchValue);
+    if (searchValue.length > 2) {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`
+      );
+      const data = await response.json();
+      console.log(data);
+      console.log(data.drinks[0]["strDrink"]);
+      setDrinks([...data.drinks[0]["strDrink"]]);
+    }
+  }
+
   return (
-    <div>
-      <ul className="list-group">
-        {props.drinks.map((drink) => (
-          <DrinkItem key={drink.idDrink} item={drink} />
-        ))}
-      </ul>
-    </div>
+    <>
+      <label>Sök här:</label>
+      <input
+        className="User-input"
+        placeholder="Sök här efter drinkar här.."
+        type="text"
+        onChange={onChange}
+      ></input>
+      <div>
+        <ul className="list-group">
+          <li className="list-group-item">{drinks}</li>
+        </ul>
+      </div>
+    </>
   );
 };
 
