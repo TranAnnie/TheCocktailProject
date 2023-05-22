a1e923ccc857e0aee8177f6cee6281d6bbb0a32c
TranAnnie <77334244+TranAnnie@users.noreply.github.com>
2023-05-22 13:57:10 +0200

    Added props

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,14 +1,11 @@
 import DrinkItem from "./DrinkItem";
-import { useState } from "react";
 
-const DrinkList = () => {
-
-
+const DrinkList = (props) => {
   return (
     <div>
       <ul className="list-group">
         {props.drinks.map((drink) => (
-          <DrinkItem key={drink.idDrink} item={drink} value={drink}  />
+          <DrinkItem key={drink.idDrink} item={drink} value={drink} />
         ))}
       </ul>
     </div>
